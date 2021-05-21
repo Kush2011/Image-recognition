fe7f0edccd738b8002124fd4eb522ae711944443
Kush2011 <74670716+Kush2011@users.noreply.github.com>
2021-05-21 20:51:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,26 @@ function take_snapshot(){
 }
 
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_uF9ZEL/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EJMnXbk-6/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded');
+}
+
+function result_of_app()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("Object").innerHTML = results[0].label;
+        document.getElementById("Accuracy").innerHTML = results[0].confidence.toFixed(4);
+    }
 }
